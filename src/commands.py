71b180c40964dfa00ceab839b8bcde1caa0c4754
Chemtails <PKbonesaw@gmail.com>
2017-04-05 16:22:01 +0200

    Update commands.py

--- src/commands.py
+++ src/commands.py
@@ -53,6 +53,7 @@ obey_dict = {
         'Garta': 'No.',
         'Wedge': 'I obey... wait, are you Wedge from the mana source:tm:?',
         'Tatters': 'I won\'t obey, because people still refuse to pronounce Ghave as Gah-Vay... Sometimes Wizards is wrong. That \'H\' is there for a reason!'
+        'Chemtails': 'I Obey, Chemtails, Don\'t hit me again please',
 }
 
 # ---------------------------
